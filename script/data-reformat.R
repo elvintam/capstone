5e49bc5341301e7e58c29d9ca332a161d7e9fada
elvintam <siochi@gmail.com>
2021-02-01 12:11:34 +0800

    data reformat data stats

---
+++ script/data-reformat.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(dslabs)
+library(caret)
+library(data.table)
+
+head(edx)
+
+temp <- edx
+
+temp <- temp %>% mutate(genrescount = str_count(edx$genres, pattern = "\\|") + 1) %>%
+  separate(genres, into = c("genres1", "genres2", "genres3", 
+                            "genres4","genres5", "genres6",
+                            "genres7", "genres8"), 
+                  sep = "\\|", remove = TRUE)
+
+##genres selection
+temp %>% filter_at(vars(starts_with("genres")), any_vars(. == "Sci-Fi"))
