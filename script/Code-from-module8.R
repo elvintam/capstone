df07a16b19ca449ff5e11674ecc7a05af7ce3e19
elvintam <siochi@gmail.com>
2021-02-02 14:01:47 +0800

    updated model 8 code

--- script/Code-from-module8.R
+++ script/Code-from-module8.R
@@ -15,8 +15,8 @@ library(caret)
 set.seed(755)
 test_index <- createDataPartition(y = movielens$rating, times = 1,
                                   p = 0.2, list = FALSE)
-train_set <- movielens[-test_index,]
 test_set <- movielens[test_index,]
+train_set <- movielens[-test_index,]
 
 test_set <- test_set %>% 
   semi_join(train_set, by = "movieId") %>%
@@ -24,3 +24,62 @@ test_set <- test_set %>%
 
 ### partition creation
 
+
+### avg, movie specific, user specific
+
+mu_hat <- mean(train_set$rating)
+mu_hat
+
+naive_rmse <- RMSE(test_set$rating, mu_hat)
+naive_rmse
+
+predictions <- rep(2.5, nrow(test_set))
+RMSE(test_set$rating, predictions)
+
+rmse_results <- data_frame(method = "Just the average", RMSE = naive_rmse)
+
+# fit <- lm(rating ~ as.factor(userId), data = movielens)
+mu <- mean(train_set$rating) 
+movie_avgs <- train_set %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = mean(rating - mu))
+
+movie_avgs %>% qplot(b_i, geom ="histogram", bins = 10, data = ., color = I("black"))
+
+predicted_ratings <- mu + test_set %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  .$b_i
+
+model_1_rmse <- RMSE(predicted_ratings, test_set$rating)
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method="Movie Effect Model",
+                                     RMSE = model_1_rmse ))
+
+rmse_results %>% knitr::kable()
+
+train_set %>% 
+  group_by(userId) %>% 
+  summarize(b_u = mean(rating)) %>% 
+  filter(n()>=100) %>%
+  ggplot(aes(b_u)) + 
+  geom_histogram(bins = 30, color = "black")
+
+# lm(rating ~ as.factor(movieId) + as.factor(userId))
+user_avgs <- test_set %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating - mu - b_i))
+
+predicted_ratings <- test_set %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = mu + b_i + b_u) %>%
+  .$pred
+
+model_2_rmse <- RMSE(predicted_ratings, test_set$rating)
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method="Movie + User Effects Model",  
+                                     RMSE = model_2_rmse ))
+rmse_results %>% knitr::kable()
+
+### avg, movie specific, user specific
