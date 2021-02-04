d0bdc845c7af9661f12476f915b66d027ed24b9a
elvintam <siochi@gmail.com>
2021-02-04 13:54:08 +0800

    added mu, bi

--- script/data-reformat.R
+++ script/data-reformat.R
@@ -14,39 +14,50 @@ temp <- temp %>% separate(title, into = c("title", "year"), sep = "\\s\\((?=[0-9
   
 head(temp)
 
+summary(temp)
+
 ##genres selection
 temp %>% filter_at(vars(starts_with("genres")), any_vars(. == "Sci-Fi"))
 
 ##avg rating for genre
 #edx %>% filter(str_detect(genres, "Drama")) %>% summarise(mean(rating))
 
-genrelist <- unique(temp$genres)
-
-genreavgrating <- temp %>% group_by(genres) %>%
-  summarize(n = n(), avg = mean(rating), se = sd(rating)/sqrt(n()))
-
-genreavgrating <- as.data.frame(genreavgrating) 
-
-genreavgrating %>% arrange(desc(n))
-
-remove(temp)
 
 ### partition creation
 set.seed(23456)
-test_index <- createDataPartition(y = edx$rating, times = 1,
+test_index <- createDataPartition(y = temp$rating, times = 1,
                                   p = 0.2, list = FALSE)
-test_set <- edx[test_index,]
-train_set <- edx[-test_index,]
+test_set <- temp[test_index,]
+train_set <- temp[-test_index,]
 
 test_set <- test_set %>% 
   semi_join(train_set, by = "movieId") %>%
   semi_join(train_set, by = "userId")
 
+rm(temp)
+rm(test_index)
 ### partition creation
 
-edx %>% summarise(n_distinct(movieId))
-edx %>% summarise(n_distinct(genres))
+### create genre avg rating table
+genreavgrating <- train_set %>% group_by(genres) %>%
+  summarize(n = n(), avg = mean(rating), se = sd(rating)/sqrt(n())) %>% 
+  arrange(desc(n))
 
-edx %>% distinct(genres)
+head(genreavgrating)
 
-rm(movielens)
+### create genre avg rating table
+
+mu_hat <- mean(train_set$rating)
+mu_hat
+
+naive_rmse <- RMSE(mu_hat, test_set$rating)
+naive_rmse
+
+mu <- mean(train_set$rating) 
+movie_avgs <- train_set %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = mean(rating - mu))
+
+head(movie_avgs)
+
+
