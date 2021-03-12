bde1437dd2c6f89f1d7f844f1d8e3d2ccaf9227d
elvintam <siochi@gmail.com>
2021-03-12 23:42:15 +0800

    Finalized

--- script/submission.R
+++ script/submission.R
@@ -3,15 +3,51 @@ library(caret)
 library(data.table)
 library(lubridate)
 
-### pre-processing
+
+#Data Loading
+
+ratings <- fread(text = gsub("::", "\t", readLines("ml-10M100K/ratings.dat")),
+                 col.names = c("userId", "movieId", "rating", "timestamp"))
+
+movies <- str_split_fixed(readLines("ml-10M100K/movies.dat"), "\\::", 3)
+colnames(movies) <- c("movieId", "title", "genres")
+
+# if using R 4.0 or later:
+movies <- as.data.frame(movies) %>% mutate(movieId = as.numeric(movieId),
+                                           title = as.character(title),
+                                           genres = as.character(genres))
+
+
+movielens <- left_join(ratings, movies, by = "movieId")
+
+# Validation set will be 10% of MovieLens data
+set.seed(1, sample.kind="Rounding") # if using R 3.5 or earlier, use `set.seed(1)`
+test_index <- createDataPartition(y = movielens$rating, times = 1, p = 0.1, list = FALSE)
+edx <- movielens[-test_index,]
+temp <- movielens[test_index,]
+
+# Make sure userId and movieId in validation set are also in edx set
+validation <- temp %>% 
+  semi_join(edx, by = "movieId") %>%
+  semi_join(edx, by = "userId")
+
+# Add rows removed from validation set back into edx set
+removed <- anti_join(temp, validation)
+edx <- rbind(edx, removed)
+
+#rm(dl, ratings, movies, test_index, temp, movielens, removed)
+rm(ratings, movies, test_index, temp, movielens, removed)
+
+
+#Data Cleaning
 temp <- edx
 
 temp <- temp %>% separate(title, into = c("title", "year"), sep = "\\s\\((?=[0-9]{4}\\))", remove = TRUE) %>%
   mutate(year = as.numeric(str_sub(year, 1, 4))) %>%
   mutate(date = as_datetime(timestamp)) %>% select(-timestamp)
-### end pre-processing
 
-### partition creation
+
+#Create Data Partition
 set.seed(12345, sample.kind="Rounding")
 test_index <- createDataPartition(y = temp$rating, times = 1,
                                   p = 0.2, list = FALSE)
@@ -23,25 +59,31 @@ test_set <- test_set %>%
   semi_join(train_set, by = "userId")
 
 rm(temp, test_index)
-### end partition creation
 
+
+### data exploration
+
+#Average Rating for all movie and all user
 mu <- mean(train_set$rating)
 
-maxyear <- max(train_set$year) + 1
 #use year + 1 as end year to calculate Rate per Year
+maxyear <- max(train_set$year) + 1
 
-l <- 2.9
 #cross validated with train set
+l <- 2.9
 
+# movie specific effect
 movie_avgs <- train_set %>%
   group_by(movieId) %>%
   summarize(b_i = sum(rating - mu) / (n() + l))
 
+# user specific effect
 user_avgs <- train_set %>%
   left_join(movie_avgs, by="movieId") %>%
   group_by(userId) %>%
   summarize(b_u = sum(rating - mu - b_i) / (n() + l))
 
+# genre specific effect
 genre_avgs <- train_set %>%
   left_join(movie_avgs, by="movieId") %>%
   left_join(user_avgs, by="userId") %>%
@@ -60,6 +102,7 @@ fit_rateperyear <- train_set %>%
   mutate(rateperyear = n/years) %>%
   lm(rating ~ rateperyear, data = .)
 
+## Rate per Year specific effect
 rateperyear <- train_set %>%
   left_join(movie_avgs, by="movieId") %>%
   left_join(user_avgs, by="userId") %>%
@@ -77,6 +120,7 @@ rateperyear <- train_set %>%
 
 
 ### test set
+#Prediction Restraint 0 to 5
 
 predicted_ratings <- test_set %>%
   left_join(movie_avgs, by="movieId") %>%
@@ -92,6 +136,7 @@ RMSE(predicted_ratings, test_set$rating)
 
 
 ### validation set
+#Prediction Restraint 0 to 5
 
 temp <- validation %>%
   semi_join(train_set, by = "movieId") %>%
@@ -106,6 +151,8 @@ predicted_ratings <- temp %>%
   mutate(pred = ifelse(pred <0, 0, ifelse(pred >5 , 5, pred))) %>%
   pull(pred)
 
+
+#NA Handling
 removed1 <- validation %>%
   anti_join(temp, by = "movieId") 
 
